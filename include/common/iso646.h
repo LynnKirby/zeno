b093464231f63dcdf8ee5db7b98c2a939d0a6826
Lynn Kirby <lynn@arrak.is>
2020-02-19 02:22:39 -0500

    iso646.h: Implement header

---
+++ include/common/iso646.h
@@ -0,0 +1,25 @@
+/*
+ * SPDX-License-Identifier: CC0-1.0
+ * iso646.h standard C header
+ */
+
+#ifndef _ISO646_H
+#define _ISO646_H
+
+#ifndef __cplusplus
+
+#define and    &&
+#define and_eq &=
+#define bitand &
+#define bitor  |
+#define compl  ~
+#define not    !
+#define not_eq !=
+#define or     ||
+#define or_eq  |=
+#define xor    ^
+#define xor_eq ^=
+
+#endif
+
+#endif

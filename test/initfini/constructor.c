412c1d2f7c33cab71f47143a3e8af0664f7ed6d9
Lynn Kirby <lynn@arrak.is>
2020-02-19 12:52:19 -0500

    test: Update some tests with expected output

--- test/initfini/constructor.c
+++ test/initfini/constructor.c
@@ -1,4 +1,5 @@
 #include "test.h"
+#include <stdio.h>
 
 static int value = 0;
 
@@ -6,16 +7,19 @@ __attribute__((constructor)) static void ctor()
 {
     assert(value == 0);
     value = 1;
+    puts("ctor\n");
 }
 
 int main()
 {
     assert(value == 1);
     value = 2;
+    puts("main\n");
     return 0;
 }
 
 __attribute__((destructor)) static void dtor()
 {
     assert(value == 2);
+    puts("dtor\n");
 }

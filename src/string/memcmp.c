2fabcba5737073e77a10a01b39026a86dbd5470c
Lynn Kirby <lynn@arrak.is>
2020-02-19 08:24:29 -0500

    memcmp: Fix const-ness in implementation types

--- src/string/memcmp.c
+++ src/string/memcmp.c
@@ -4,8 +4,8 @@
 
 int memcmp(const void *s1, const void *s2, size_t n)
 {
-    unsigned char *c1 = s1;
-    unsigned char *c2 = s2;
+    const unsigned char *c1 = s1;
+    const unsigned char *c2 = s2;
 
     while (n != 0) {
         if (*c1 != *c2) {

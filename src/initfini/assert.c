3e332cd14fe461b6f553f6b0b5f5c54dbad63c7c
Lynn Kirby <lynn@arrak.is>
2020-02-19 12:50:55 -0500

    assert: Write to stderr on failure
    Before we were using a hacky way of indicating failure (things like
    __builtin_trap()) but now we can have actual assertion messages!

--- src/initfini/assert.c
+++ src/initfini/assert.c
@@ -1,14 +1,42 @@
 /* SPDX-License-Identifier: CC0-1.0 */
-
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 20 characters + null */
+#define UTOA_BUF_SIZE 21
+
+static char *utoa(unsigned value, char *buf)
+{
+    char *ptr = buf + UTOA_BUF_SIZE - 1;
+    *ptr      = '\0';
+
+    do {
+        ptr -= 1;
+        *ptr = '0' + (value % 10);
+        value /= 10;
+    } while (value != 0);
+
+    return ptr;
+}
 
 _Noreturn void __assert_fail(
-    const char *expr, const char *file, int line, const char *func)
+    const char *assertion, const char *file, int line, const char *func)
 {
-    /* FIXME: Fake assertion until IO is implemented. */
-#ifdef _MSC_VER
-    __debugbreak();
-#else
-    __builtin_trap();
-#endif
+    char buf[UTOA_BUF_SIZE];
+
+    /* Message similar to: "a.c:10: foobar: Assertion a == b failed."
+     * TODO: replace with fprintf when implemented. */
+    fputs(file, stderr);
+    fputs(":", stderr);
+    fputs(utoa((unsigned) line, buf), stderr);
+    fputs(": ", stderr);
+    fputs(func, stderr);
+    fputs(": ", stderr);
+    fputs("Assertion ", stderr);
+    fputs(assertion, stderr);
+    fputs(" failed.\n", stderr);
+
+    /* TODO: Should flush streams and call abort() when implemented. */
+    _Exit(127);
 }

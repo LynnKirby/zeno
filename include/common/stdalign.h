7508b69c01eb78520073ad785cee715ff5f66ba7
Lynn Kirby <lynn@arrak.is>
2020-02-19 02:24:51 -0500

    stdalign.h: Implement header

---
+++ include/common/stdalign.h
@@ -0,0 +1,17 @@
+/*
+ * SPDX-License-Identifier: CC0-1.0
+ * stdalign.h standard C header
+ */
+
+#ifndef _STDALIGN_H
+#define _STDALIGN_H
+
+#ifndef __cplusplus
+#define alignas _Alignas
+#define alignof _Alignof
+#endif
+
+#define __alignas_is_defined 1
+#define __alignof_is_defined 1
+
+#endif

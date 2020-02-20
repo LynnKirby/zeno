0a4c0547d2644508e45bd68e69b74098d1c703a7
Lynn Kirby <lynn@arrak.is>
2020-02-20 07:56:53 -0500

    toolchain: Find Clang toolchain in LLVM directory
    On Linux, versioned LLVM tools get symlinked into /usr/bin. For example
    clang-9 is actually located at /usr/lib/llvm-9/bin/clang but it is
    symlinked as /usr/bin/clang-9. When CMake uses the /usr/bin path, that's
    also where it looks for other tools. This can lead to inconsistent
    versions used; such as clang-9 and the system default llvm-ar (which is
    likely a much older version).
    
    This commit makes CMake use the real path of the Clang executable.

--- cmake/toolchains/base/clang.cmake
+++ cmake/toolchains/base/clang.cmake
@@ -17,11 +17,22 @@ endif()
 
 message(STATUS "CLANG_TRIPLE=${CLANG_TRIPLE}")
 
-set(CMAKE_C_COMPILER ${CLANG_EXECUTABLE})
+# We need the absolute path so that CMake will find tools correctly.
+#
+# On Linux, versioned LLVM tools get symlinked into /usr/bin. For example
+# clang-9 is actually located at /usr/lib/llvm-9/bin/clang but it is symlinked
+# as /usr/bin/clang-9. When CMake uses the /usr/bin path, that's also where it
+# looks for other tools. This can lead to inconsistent versions used; such as
+# clang-9 and the system default llvm-ar (which is likely a much older version).
+get_filename_component(clang_abspath "${CLANG_EXECUTABLE}" REALPATH)
+
+# These variables need to be forced otherwise CMake will overwrite them in the
+# second part of the configuration phase. CMake overwrites them with the
+# original, non-absolute path for some reason.
+set(CMAKE_C_COMPILER ${clang_abspath} CACHE FILEPATH "" FORCE)
+set(CMAKE_CXX_COMPILER ${clang_abspath} CACHE FILEPATH "" FORCE)
+set(CMAKE_ASM_COMPILER ${clang_abspath} CACHE FILEPATH "" FORCE)
+
 set(CMAKE_C_COMPILER_TARGET ${CLANG_TRIPLE})
-
-set(CMAKE_CXX_COMPILER ${CLANG_EXECUTABLE})
 set(CMAKE_CXX_COMPILER_TARGET ${CLANG_TRIPLE})
-
-set(CMAKE_ASM_COMPILER ${CLANG_EXECUTABLE})
 set(CMAKE_ASM_COMPILER_TARGET ${CLANG_TRIPLE})

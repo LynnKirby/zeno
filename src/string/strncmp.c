ecd3fe721adbf229b83b144d9dbf48d2c78d5822
Lynn Kirby <lynn@arrak.is>
2020-02-20 21:32:11 -0500

    strncmp: Fix bug when length limit is reached

--- src/string/strncmp.c
+++ src/string/strncmp.c
@@ -4,11 +4,15 @@
 
 int strncmp(const char *s1, const char *s2, size_t n)
 {
-    while (*s1 == *s2 && *s1 != '\0' && n > 0) {
-        s1++;
-        s2++;
+    unsigned char *c1 = (unsigned char*)s1;
+    unsigned char *c2 = (unsigned char*)s2;
+
+    while (n > 0 && *c1 == *c2 && *c1 != '\0') {
+        c1++;
+        c2++;
         n--;
     }
 
-    return (unsigned char)*s1 - (unsigned char)*s2;
+    if (n == 0) return 0;
+    return *c1 - *c2;
 }
--- test/string/strncmp.c
+++ test/string/strncmp.c
@@ -4,12 +4,28 @@
 int main(void)
 {
     assert(strncmp("foo", "foo", 10) == 0);
-
     assert(strncmp("abc", "xyzx", 10) < 0);
     assert(strncmp("xyz", "abcx", 10) > 0);
-
-    assert(strncmp("aaa", "aabx", 2) < 0);
+    assert(strncmp("aaa", "aabx", 2) == 0);
     assert(strncmp("baa", "aabx", 2) > 0);
+    assert(strncmp("aaa", "aabx", 3) < 0);
+
+    char abcde[]    = "abcde";
+    char abcdx[]    = "abcdx";
+    char cmpabcde[] = "abcde\0f";
+    char cmpabcd_[] = "abcde\xfc";
+    char empty[]    = "";
+    char x[]        = "x";
+    assert(strncmp(abcde, cmpabcde, 5) == 0);
+    assert(strncmp(abcde, cmpabcde, 10) == 0);
+    assert(strncmp(abcde, abcdx, 5) < 0);
+    assert(strncmp(abcdx, abcde, 5) > 0);
+    assert(strncmp(empty, abcde, 5) < 0);
+    assert(strncmp(abcde, empty, 5) > 0);
+    assert(strncmp(abcde, abcdx, 4) == 0);
+    assert(strncmp(abcde, x, 0) == 0);
+    assert(strncmp(abcde, x, 1) < 0);
+    assert(strncmp(abcde, cmpabcd_, 10) < 0);
 
     return 0;
 }

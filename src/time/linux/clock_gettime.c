bf03969ba93e112268b0565a3f11960f7fb5c488
Lynn Kirby <lynn@arrak.is>
2020-02-26 21:28:27 -0500

    time: fix type used to get Linux kernel time
    We were using the wrong size of struct when calling clock_gettime64. It
    should have both members as int64_t but we were using the public struct
    timespec from fenv.h which has a long tv_nsec member. The syscall ends
    up writing beyond the end of the struct.

--- src/time/linux/clock_gettime.c
+++ src/time/linux/clock_gettime.c
@@ -5,18 +5,24 @@
 #include <sys/syscall.h>
 #include <time.h>
 
+#ifdef SYS_clock_gettime64
 struct timespec32 {
-    uint32_t tv_sec;
-    long tv_nsec;
+    int32_t tv_sec;
+    int32_t tv_nsec;
+};
+
+struct timespec64 {
+    int64_t tv_sec;
+    int64_t tv_nsec;
 };
 
 int clock_gettime(clockid_t clk, struct timespec *ts)
 {
     long r;
+    struct timespec64 ts64;
 
-#ifdef SYS_clock_gettime64
-    /* Try to use 64-bit version if it's defined for this architecture. */
-    r = __syscall(SYS_clock_gettime64, clk, (long)ts);
+    /* Try to use 64-bit version. */
+    r = __syscall(SYS_clock_gettime64, clk, (long)&ts64);
 
     /* clock_gettime64 is from Linux 5.1 (May 2019) so fall back to the 32-bit
      * version if we don't have it. */
@@ -27,10 +33,18 @@ int clock_gettime(clockid_t clk, struct timespec *ts)
             ts->tv_sec  = ts32.tv_sec;
             ts->tv_nsec = ts32.tv_nsec;
         }
+    } else if (r == 0) {
+        ts->tv_sec = ts64.tv_sec;
+        /* It's a 64-bit value but the kernel always returns a normalized value
+         * where tv_nsec is less than 1 second. */
+        ts->tv_nsec = (int32_t)ts64.tv_nsec;
     }
-#else
-    r = __syscall(SYS_clock_gettime, clk, (long)ts);
-#endif
 
     return __syscall_handle_errno(r);
 }
+#else
+int clock_gettime(clockid_t clk, struct timespec *ts)
+{
+    __syscall_handle_errno(__syscall(SYS_clock_gettime, clk, (long)ts));
+}
+#endif
